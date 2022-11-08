60e40be71e8e018096b28a28cb5edc8682b8828d
LuisSevilha <luissevilhadev@gmail.com>
2022-11-08 14:59:36 -0300

    Tudo funcionando só falta conectar ao bda.

--- src/sender.ts
+++ src/sender.ts
@@ -1,3 +1,5 @@
+import parsePhoneNumber, {isValidPhoneNumber} from "libphonenumber-js"
+import { PhoneNumber } from "libphonenumber-js"
 import { start } from "repl"
 import { create, Whatsapp, Message, SocketState } from "venom-bot"
 
@@ -8,13 +10,26 @@ class Sender{
         this.initialize()
     }
     
+    async sendText(to: string, body: string){
+        if(!isValidPhoneNumber(to, 'BR')){
+            throw new Error('this number is not valid !!')
+        }
+
+        let phoneNumber = parsePhoneNumber(to, 'BR')
+          ?.format('E.164')
+          ?.replace('+','') as string
+          phoneNumber = phoneNumber+('@c.us') 
+
+        await this.client.sendText(phoneNumber, body)
+    }
+
     private initialize(){
-        const qr = (base64Qrimg: string, asciiQR: String) => {
+        const qr = (base64Qrimg: string, asciiQR: string) => {
             console.log(asciiQR);
         }
 
         const start = (client: Whatsapp) => {
-            this.client = client
+            this.client = client;
         }
 
         create('sender-temp', qr)
